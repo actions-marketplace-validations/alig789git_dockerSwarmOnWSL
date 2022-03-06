1164afa75d66c98f7cc3aaa5c21a09822e0da6fc
Ihor Aleksandrov <ialeks@softserveinc.com>
2022-03-06 23:26:28 +0200

    first commit

---
+++ install.ps1
@@ -0,0 +1,18 @@
+﻿$wslpath = 'C:\WSL\'
+###+++++++ Install WSL +++++++###
+
+###+++++++ Install Debian +++++++###
+
+wsl --install -d Debian
+wsl -d Debian -u root "./scripts/setDebianDistro.sh"
+
+###+++++++ Install Docker +++++++###
+
+wsl -d Debian -u root "./scripts/addDocker2Debian.sh"
+
+###+++++++ Export Debian +++++++###
+
+if (Test-Path -Path $wslpath){
+    Remove-Item -Path $wslpath -Recurse -Force -WhatIf
+}
+#wsl
---
+++ scripts/setDebianDistro.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+apt-get update
+apt-get upgrade -y
+useradd -p P@ssw0rd -m dockeradm
